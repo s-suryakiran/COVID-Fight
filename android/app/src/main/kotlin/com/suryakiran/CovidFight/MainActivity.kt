c1c66d211e01080cdfd6067593c78f50a085c15e
Surya Kiran <suryakiran@Suryas-MacBook-Air.local>
2020-05-18 21:43:42 +0530

    First Commit

---
+++ android/app/src/main/kotlin/com/suryakiran/CovidFight/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.suryakiran.CovidFight
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+void main() {
+  runApp(MaterialApp(home: Scaffold(
+    
+    appBar: AppBar(
+      title: Text('Covid Fight'),
+      backgroundColor: Colors.red,
+    ),
+    body: CovidFight(),
+  ),));
+}
+
+class CovidFight extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      alignment: Alignment.bottomRight,
+      child: FloatingActionButton(
+          child: Icon(Icons.message),
+
+      ),
+    );
+  }
+}
