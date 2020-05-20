adc5781dac7ab494cfc4d5eae214ffbb194fa3aa
s-suryakiran <surya.kiran2872@gmail.com>
2020-05-20 13:09:56 +0530

    another

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Covid Fight',
       theme: ThemeData(
           scaffoldBackgroundColor: kBackgroundColor,
