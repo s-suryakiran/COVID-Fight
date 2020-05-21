052a86947266de75e9c3b2d08c2521a64402f209
s-suryakiran <surya.kiran2872@gmail.com>
2020-05-21 20:17:13 +0530

    Merge branch 'master' of https://github.com/s-suryakiran/Covid-Flutter

---
+++ lib/self_diagnosis.dart
@@ -0,0 +1,9 @@
+class SelfDiagnosis
+{
+  String questionTitle,choice1,choice2,choice3,choice4,choice5;
+
+  SelfDiagnosis({this.questionTitle, this.choice1, this.choice2,this.choice3,this.choice4,this.choice5});
+
+
+}
+
