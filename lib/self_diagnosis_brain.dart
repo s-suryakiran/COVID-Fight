d34f7c969bf5ee6f7f384a2cf5437242cb21b269
ALCINA <alcinasharon@gmail.com>
2020-05-22 15:42:00 +0530

    diagnosis brain done

--- lib/self_diagnosis_brain.dart
+++ lib/self_diagnosis_brain.dart
@@ -1,11 +1,15 @@
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 import 'self_diagnosis.dart';
 
 class StoryBrain {
   List<int> answer;
   int currentQuestion;
   int nextQuestion;
-  bool redFlag,stopFlag=false,lead = false;
-  int age;
+  bool redFlag, stopFlag = false, lead = false;
+  int age, fever;
+
+//  TODO:UPDATE FEVER VALUE IN QUESTIONS 11,26,31
 
   //type 1=> yes or no
   //type 2=> single choice selection
@@ -445,21 +449,31 @@ class StoryBrain {
     //57
     //TODO
     SelfDiagnosis(type: 4, questionTitle: 'Result', choice1: 'Call Doctor.'),
-
   ];
 
-  String getQuestion(){
+  String getQuestion() {
     return _questions[currentQuestion].questionTitle;
   }
 
   void nextStory() {
-
     switch (currentQuestion) {
+      case 0:
+        {
+          age = answer[0];
+          nextQuestion = 1;
+        }
+        break;
+      case 1:
+        {
+          if (answer.contains(2)) redFlag = true;
+          nextQuestion = 2;
+        }
+        break;
       case 3:
         {
           switch (answer) {
             case [-1]:
-             nextQuestion = 4;
+              nextQuestion = 4;
               break;
             case [0, 1]:
               nextQuestion = 11;
@@ -468,126 +482,402 @@ class StoryBrain {
               nextQuestion = 31;
               break;
             case [1]:
-              nextQuestion=32;
+              nextQuestion = 32;
               break;
             default:
               {
-                if (!answer.contains(0) && answer==[2])
+                if (!answer.contains(0) && answer == [2])
                   nextQuestion = 46;
-                else if (answer==[0,2] || answer==[0,1,2])
+                else if (answer == [0, 2] || answer == [0, 1, 2])
                   nextQuestion = 26;
               }
           }
-        }break;
-
-      case 4:{
-        if(answer.contains(7))
-          redFlag=true;
-        if(answer==[-1] || answer==[0])
-          nextQuestion =5;
-        //TODO please check
-        else if(!answer.contains(0))
-          nextQuestion = 6;
-      }break;
-
-      case 5:{
-        switch(answer){
-          case [0]:
-          case[1]:
-          case[2]:{
-            nextQuestion=9;
-            redFlag = true;
-            stopFlag = true;
-        }break;
-          case[3]:
-          case[4]:{
-            nextQuestion=10;
-            stopFlag=true;
-          }break;
         }
-      }break;
+        break;
 
-      case 6:{
-        switch(answer){
-          case [0]:
-          case[1]:
-          case[2]:{
-            nextQuestion=7;
-            redFlag = true;
-            stopFlag = true;
-          }break;
-          case[3]:
-          case[4]:{
-            nextQuestion=8;
-            stopFlag=true;
-          }break;
+      case 4:
+        {
+          if (answer.contains(7)) redFlag = true;
+          if (answer == [-1] || answer == [0])
+            nextQuestion = 5;
+          //TODO please check
+          else if (!answer.contains(0)) nextQuestion = 6;
         }
-      }break;
+        break;
 
-//      TODO: handle questions 11,12,13,14
-      case 15:{
-        if(answer.contains(7))
-          redFlag=true;
-        if(answer==[-1] || answer==[0])
-          nextQuestion =21;
-        //TODO please check
-        else if(!answer.contains(0))
-          nextQuestion = 16;
-      }break;
-
-      case 16:{
-        nextQuestion = (answer==[1])?20:17;
-        if(nextQuestion==20)
-          stopFlag=true;
-      }break;
-
-      case 21:{
-        nextQuestion = (answer==[1])?20:22;
-        if(nextQuestion==20)
-          stopFlag=true;
-      }break;
-
-      case 17:{
-        switch(answer){
-          case [0]:
-          case[1]:
-          case[2]:{
-            nextQuestion=18;
-            redFlag = true;
-            stopFlag = true;
-          }break;
-          case[3]:
-          case[4]:{
-            nextQuestion=19;
-            stopFlag=true;
-          }break;
+      case 5:
+        {
+          switch (answer) {
+            case [0]:
+            case [1]:
+            case [2]:
+              {
+                nextQuestion = 9;
+                redFlag = true;
+                stopFlag = true;
+              }
+              break;
+            case [3]:
+            case [4]:
+              {
+                nextQuestion = 10;
+                stopFlag = true;
+              }
+              break;
+          }
         }
-      }break;
+        break;
 
-      case 22:{
-        switch(answer){
-          case [0]:
-          case[1]:
-          case[2]:{
-            nextQuestion=23;
-            redFlag = true;
-            stopFlag = true;
-          }break;
-          case[3]:
-          case[4]:{
-            nextQuestion=24;
-            if(!(redFlag==true && age>65))
-              stopFlag = true;
-          }break;
+      case 6:
+        {
+          switch (answer) {
+            case [0]:
+            case [1]:
+            case [2]:
+              {
+                nextQuestion = 7;
+                redFlag = true;
+                stopFlag = true;
+              }
+              break;
+            case [3]:
+            case [4]:
+              {
+                nextQuestion = 8;
+                stopFlag = true;
+              }
+              break;
+          }
         }
-      }break;
-      case 24:{
-        nextQuestion = 25;
-        stopFlag=true;
-      }break;
+        break;
 
-    }//switch ends
-  }//function ends
+      case 11:
+        {
 
+          if (answer.contains(2)) {
+            lead = true;
+            fever = 45;
+          }else
+            fever = 30;
+          nextQuestion = 12;
+        }
+        break;
+      case 12:
+        {
+          if (answer == [0]) //if yes
+            lead = true;
+          nextQuestion = 13;
+        }
+        break;
+      case 13:
+        {
+          if (answer == [0]) //if yes
+            lead = true;
+          nextQuestion = 14;
+        }
+        break;
+      case 14:
+        {
+          if (answer == [0]) //if yes
+            lead = true;
+          if (redFlag == true || fever > 40 || lead == true)
+            nextQuestion = 30;
+          else
+            nextQuestion = 15;
+          lead = false;
+        }
+        break;
+      case 15:
+        {
+          if (answer.contains(7)) redFlag = true;
+          if (answer == [-1] || answer == [0])
+            nextQuestion = 21;
+          //TODO please check
+          else if (!answer.contains(0)) nextQuestion = 16;
+        }
+        break;
+
+      case 16:
+        {
+          nextQuestion = (answer == [1]) ? 20 : 17;
+          if (nextQuestion == 20) stopFlag = true;
+        }
+        break;
+
+      case 21:
+        {
+          nextQuestion = (answer == [1]) ? 20 : 22;
+          if (nextQuestion == 20) stopFlag = true;
+        }
+        break;
+
+      case 17:
+        {
+          switch (answer) {
+            case [0]:
+            case [1]:
+            case [2]:
+              {
+                nextQuestion = 18;
+                redFlag = true;
+                stopFlag = true;
+              }
+              break;
+            case [3]:
+            case [4]:
+              {
+                nextQuestion = 19;
+                stopFlag = true;
+              }
+              break;
+          }
+        }
+        break;
+
+      case 22:
+        {
+          switch (answer) {
+            case [0]:
+            case [1]:
+            case [2]:
+              {
+                nextQuestion = 23;
+                redFlag = true;
+                stopFlag = true;
+              }
+              break;
+            case [3]:
+            case [4]:
+              {
+                nextQuestion = 24;
+                if (!(redFlag == true && age > 65)) stopFlag = true;
+              }
+              break;
+          }
+        }
+        break;
+      case 24:
+        {
+          nextQuestion = 25;
+          stopFlag = true;
+        }
+        break;
+
+      case 26:
+        {
+          if(answer.contains(2))
+            fever = 45;
+          else
+            fever = 35;
+          nextQuestion = 27;
+        }
+        break;
+      case 27:
+        nextQuestion = 28;
+        break;
+      case 28:
+        nextQuestion = 29;
+        break;
+      case 29:
+        {
+          nextQuestion = 30;
+          stopFlag = true;
+        }
+        break;
+//      TODO:HANDLE 31,32,33,34
+      case 31:
+        {
+          if(answer.contains(2))
+            fever = 45;
+          else
+            fever = 35;
+          nextQuestion = 32;
+        }
+        break;
+      case 32:
+        {
+          if (answer == [0]) //if yes
+            lead = true;
+          nextQuestion = 33;
+        }
+        break;
+      case 33:
+        {
+          if (answer == [0]) //if yes
+            lead = true;
+          nextQuestion = 34;
+        }
+        break;
+      case 34:
+        {//TODO: CHECK FLOW .. HERE FEVER IS NOT CONSIDERED AT ALL
+          if (answer == [0]) //if yes
+            lead = true;
+          if(redFlag==true || lead==true)
+            nextQuestion = 30;
+          else
+            nextQuestion = 35;
+        }
+        break;
+      case 35:
+        {
+          if (answer.contains(7)) redFlag = true;
+          if (answer == [-1] || answer == [0])
+            nextQuestion = 41;
+          //TODO please check
+          else if (!answer.contains(0)) nextQuestion = 36;
+        }
+        break;
+
+      case 36:
+        {
+          switch (answer) {
+            case [0]:
+            case [1]:
+            case [2]:
+              {
+                redFlag = true;
+                nextQuestion = 37;
+                if (fever <= 40) stopFlag = true;
+              }
+              break;
+            case [3]:
+            case [4]:
+              {
+                nextQuestion = 39;
+                if (fever <= 40 || age < 65 || redFlag == false)
+                  stopFlag = true;
+              }
+              break;
+          }
+        }
+        break;
+      case 37:
+        {
+          nextQuestion = 38;
+          stopFlag = true;
+        }
+        break;
+      case 39:
+        {
+          nextQuestion = 40;
+          stopFlag = true;
+        }
+        break;
+      case 41:
+        {
+          switch (answer) {
+            case [0]:
+            case [1]:
+            case [2]:
+              {
+                redFlag = true;
+                nextQuestion = 42;
+                if (fever <= 40) stopFlag = true;
+              }
+              break;
+            case [3]:
+            case [4]:
+              {
+                nextQuestion = 44;
+                if (fever <= 40) stopFlag = true;
+              }
+              break;
+          }
+        }
+        break;
+      case 42:
+        {
+          nextQuestion = 43;
+          stopFlag = true;
+        }
+        break;
+      case 44:
+        {
+          nextQuestion = 45;
+          stopFlag = true;
+        }
+        break;
+
+      case 46:
+        {
+          nextQuestion = 47;
+        }
+        break;
+      case 47:
+        {
+          nextQuestion = 48;
+        }
+        break;
+      case 48:
+        {
+          if (redFlag) {
+            nextQuestion = 54;
+            stopFlag = true;
+          } else
+            nextQuestion = 49;
+        }
+        break;
+
+      case 49:
+        {
+          if (answer == [-1] || answer == [0])
+            nextQuestion = 55;
+          //TODO please check
+          else if (!answer.contains(0)) nextQuestion = 50;
+        }
+        break;
+
+      case 50:
+        {
+          switch (answer) {
+            case [0]:
+            case [1]:
+            case [2]:
+              {
+                redFlag = true;
+                nextQuestion = 51;
+                if (age < 65 || redFlag == false) stopFlag = true;
+              }
+              break;
+            case [3]:
+            case [4]:
+              {
+                nextQuestion = 53;
+                stopFlag = true;
+              }
+              break;
+          }
+        }
+        break;
+      case 51:
+        {
+          nextQuestion = 52;
+          stopFlag = true;
+        }
+        break;
+      case 55:
+        {
+          switch (answer) {
+            case [0]:
+            case [1]:
+            case [2]:
+              {
+                nextQuestion = 56;
+                redFlag = true;
+                stopFlag = true;
+              }
+              break;
+            case [3]:
+            case [4]:
+              {
+                nextQuestion = 57;
+                stopFlag = true;
+              }
+              break;
+          }
+        }
+        break;
+    } //switch ends
+  } //function ends
 
 }
