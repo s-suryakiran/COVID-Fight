6d161d055cc366620c68198a41695702c16c6581
s-suryakiran <surya.kiran2872@gmail.com>
2020-05-23 20:09:22 +0530

    Merge branch 'master' of https://github.com/s-suryakiran/Covid-Flutter

--- lib/chatbot.dart
+++ lib/chatbot.dart
@@ -9,6 +9,7 @@ class _ChatBotState extends State<ChatBot> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text("Covid Assistant"),),
       body: WebView(
         initialUrl:"https://web-chat.global.assistant.watson.cloud.ibm.com/preview.html?region=eu-gb&integrationID=5be98453-a32f-4aba-b4a3-1b1e9d2cb810&serviceInstanceID=60d1e6fa-42ba-4a2e-9d4f-fae78ed68966" ,
       javascriptMode: JavascriptMode.unrestricted,
